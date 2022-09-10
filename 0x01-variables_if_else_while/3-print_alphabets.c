44c0e445669001a749a091331e7c60301abb58b5
Kipkiruilelgo <kipkiruigypson@gmail.com>
2022-09-10 02:34:10 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,41 +1,65 @@
-nclude <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
 
 /**
  *
- *  * main - print if the number is postive, zero, or negative
+ * * main - print if the number is postive, zero, or negative
  *
- *   *
+ * *
  *
- *    * Description: using the main function
+ * * Description: using the main function
  *
- *     * this program prints "Programming is positive, zero, or negative
+ * * this program prints "Programming is positive, zero, or negative
  *
- *      * Return: 0
+ * * Return: 0
  *
- *       */
+ * */
 
 int main(void)
+	
+{
+
+int n;
+
+int l;
+
+
+
+srand(time(0));
+
+n = rand() - RAND_MAX / 2;
+
+l = n % 10;
+
+
+
+if (l > 5)
 
 {
 
-	char ch;
+	printf("Last digit of %d is %d and is greater than 5\n", n, l);
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+}
 
-	{
+else if (l == 0)
+	
+{
 
-		if (ch != 'e' && ch != 'q')
+	printf("Last digit of %d is %d and is 0\n", n, l);
 
-		{
+}
 
-				putchar(ch);
+else
 
-		}
+{
 
-	}
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 
-	putchar('\n');
+}
 
-	return (0);
+return (0);
 
 }
