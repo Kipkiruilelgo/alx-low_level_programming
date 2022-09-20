d3fb13e28d2c95c8074b1dc7f4d8bf168c236699
Kipkiruilelgo <kipkiruigypson@gmail.com>
2022-09-20 09:04:48 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,28 @@
-#include "main.h"
+#include "<unistd.h"
+
+
 
 /**
- * main - print putchar
- *  Return: always 0
- */
 
-int main(void)
+* print_alphabet - prints the alphabet in lowercase, followed by a new line.
+
+*/
+
+void print_alphabet(void)
+
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
 
-    return (0);
+char letter;
+
+
+
+for (letter = 'a'; letter <= 'z'; letter++)
+
+		
+_putchar(letter);
+
+
+
+_putchar('\n');
 
 }
